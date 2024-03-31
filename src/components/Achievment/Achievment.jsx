16e2a608706c5cf81927f8ed532b6f4cc20ef9ee
Ivan Bulychev <psmivan@gmail.com>
2024-03-31 15:47:14 +0300

    Доработка

--- src/components/Achievment/Achievment.jsx
+++
@@ -1,9 +0,0 @@
-import unsuper1 from "./unsuper1.svg";
-import super1 from "./super1.svg";
-
-export default function Achievment(isAchievment) {
-  console.log(Object.values(isAchievment)[2].isAchievment);
-  return (
-    <>{Object.values(isAchievment)[2].isAchievment ? <img src={super1} alt="" /> : <img src={unsuper1} alt="" />}</>
-  );
-}
